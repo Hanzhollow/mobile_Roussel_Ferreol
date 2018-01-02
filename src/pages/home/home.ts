fe74b342f163869c895827b244767a29261cac69
Hanzhollow <hanselot.roussel@gmail.com>
2018-01-02 22:04:50 +0100

    prompts for add and close

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public alertCtrl: AlertController, public toastCtrl: ToastController) {
   }
 
+  AddPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'New task',
+      message: "Enter a name for this new task",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'New task name'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Next',
+          handler: data => {
+            console.log('Next clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  
+  }
+  ClosePrompt() {
+    let toast = this.toastCtrl.create({
+      message: 'Please select tasks to close',
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
 }
